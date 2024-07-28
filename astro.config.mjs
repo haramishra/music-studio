7a44e8c18aea66e8a4b8ba14c49927d3d87b8708
hara <haraprasad45@gmail.com>
2024-07-29 00:53:50 +0530

    components updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon(), tailwind(), sitemap()],
+});
